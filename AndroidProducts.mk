097fe5c811f61925a270f390fe1961e537b502f2
Tim Zimmermann <tim@linux4.de>
2020-04-10 09:01:11 +0200

    beyond2lte: add COMMON_LUNCH_CHOICES

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -16,3 +16,8 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_beyond2lte.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_beyond2lte-eng \
+    lineage_beyond2lte-user \
+    lineage_beyond2lte-userdebug
